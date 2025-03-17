f57c24cd95906ba755f9cb74c1e0833ad2a1959e
lcy-orangepi-max-1 <lcy@cug.edu.cn>
2025-03-18 01:26:36 +0800

    添加pid控制代码

--- ros_ws_uuv/src/px4_offboard_control/include/joy_ctrl.h
+++ ros_ws_uuv/src/px4_offboard_control/include/joy_ctrl.h
@@ -11,10 +11,44 @@
 #include "sensor_msgs/msg/joy.hpp"
 #include "px4_msgs/msg/actuator_motors.hpp"
 #include "px4_msgs/msg/battery_status.hpp"
+#include "px4_msgs/msg/vehicle_attitude.hpp"
+#include "px4_msgs/msg/sensor_baro.hpp"
+#include "matrix/math.hpp"
+
 
 using std::placeholders::_1;//传递参数的占位符，有一个参数时使用_1，有两个参数时使用_1,_2
 
+using matrix::Eulerf;
+using matrix::Quatf;
+using matrix::Matrix3f;
+using matrix::Vector3f;
+using matrix::Dcmf;
+
 using namespace std::chrono_literals;
+
+class IncPID
+{
+public:
+	float IncPIDCalc(float &CurrentPoint,float &SetPoint,float Proportion,float Integral,float Derivative) {
+	float iError=SetPoint-CurrentPoint;                                     // 计算当前误差
+	m_iIncpid += (Proportion * (iError - m_Error1)                  // P
+		+ Integral * iError                                   // I
+		+ Derivative * (iError - 2 * m_Error1 + m_Error2));  // D
+
+	m_Error2 = m_Error1;                          // 存储误差，用于下次计算
+	m_Error1 = iError;
+
+	return(m_iIncpid);                                    // 返回增量值
+	}
+	IncPID(){};
+	~IncPID(){};
+private:
+
+
+	float m_Error1 = 0;
+	float m_Error2 = 0;
+	float m_iIncpid = 0;
+};
 
 class joy_ctrl:public rclcpp::Node
 {
@@ -23,17 +57,45 @@ class joy_ctrl:public rclcpp::Node
         float _battery_voltage{14.0f};
         float _ctrl_input[8]{0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f} ;
         bool _input_update{false};
+        bool _depth_ctrl{false};
+        bool _yaw_ctrl{false};
+        bool _roll_ctrl{false};
         float _T_min{0.0f};
-        float _T_max{0.0f};
-        float _x_max{6.0f};
-        float _y_max{6.0f};
-        float _z_max{6.0f};
-        float _roll_max{1.3f};
-        float _yaw_max{4.5f};
+        float _T_max{0.0f};//实时跟新推力最大值
+        float _x_max{6.0f};//实时更新推力最小值
+        float _y_max{6.0f};//控制增益
+        float _z_max{6.0f};//控制增益
+        float _roll_max{1.3f};//控制增益
+        float _yaw_max{4.5f};//控制增益
+
+        float _depth{0.0f};
+        float _depth_set{0.5f};
+        float _roll{0.0f};
+        float _roll_set{0.0f};
+        float _yaw{0.0f};
+        float _yaw_set{0.0f};
+
+        float _depth_p{1.0f};
+        float _depth_i{0.000f};
+        float _depth_d{0.000f};
+        float _roll_p{1.00f};
+        float _roll_i{0.000f};
+        float _roll_d{0.000f};
+        float _yaw_p{1.0f};
+        float _yaw_i{0.000f};
+        float _yaw_d{0.000f};
+
         power_dis_UUV6 _power_dis;
+
+        IncPID m_IncPID_depth;
+	    IncPID m_IncPID_roll;
+	    IncPID m_IncPID_yaw;
+
     
         rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr Joy_sub_;
         rclcpp::Subscription<px4_msgs::msg::BatteryStatus>::SharedPtr BatteryStatus_sub_;
+        rclcpp::Subscription<px4_msgs::msg::VehicleAttitude>::SharedPtr VehicleAttitude_sub_;
+        rclcpp::Subscription<px4_msgs::msg::SensorBaro>::SharedPtr SensorBaro_sub_;
 
         rclcpp::Publisher<px4_msgs::msg::ActuatorMotors>::SharedPtr ActuatorMotors_pub_;
 
@@ -41,6 +103,9 @@ class joy_ctrl:public rclcpp::Node
 
         void Joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
         void BatteryStatus_callback(const px4_msgs::msg::BatteryStatus::SharedPtr msg);
+        void VehicleAttitude_callback(const px4_msgs::msg::VehicleAttitude::SharedPtr msg);
+        void SensorBaro_callback(const px4_msgs::msg::SensorBaro::SharedPtr msg);
+
 
         void Timer_Control_callback();
 
@@ -53,6 +118,8 @@ class joy_ctrl:public rclcpp::Node
         
             Joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&joy_ctrl::Joy_callback, this, _1));
             BatteryStatus_sub_ = this->create_subscription<px4_msgs::msg::BatteryStatus>("fmu/out/battery_status", qos, std::bind(&joy_ctrl::BatteryStatus_callback, this, _1));
+            VehicleAttitude_sub_ = this->create_subscription<px4_msgs::msg::VehicleAttitude>("fmu/out/vehicle_attitude", qos, std::bind(&joy_ctrl::VehicleAttitude_callback, this, _1));
+            SensorBaro_sub_ = this->create_subscription<px4_msgs::msg::SensorBaro>("fmu/out/sensor_baro", qos, std::bind(&joy_ctrl::SensorBaro_callback, this, _1));
         
             ActuatorMotors_pub_ = this->create_publisher<px4_msgs::msg::ActuatorMotors>("fmu/in/actuator_motors", 10);
         
--- ros_ws_uuv/src/px4_offboard_control/src/joy_ctrl.cpp
+++ ros_ws_uuv/src/px4_offboard_control/src/joy_ctrl.cpp
@@ -10,6 +10,9 @@ void joy_ctrl::Joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     _ctrl_input[5] = msg->axes[5];//右肩键按下：axes5 1 - -1
     _ctrl_input[6] = msg->axes[6];
     _ctrl_input[7] = msg->axes[7];
+    _depth_ctrl = msg->buttons[0];//X键按下：buttons0 1 - -1
+    _yaw_ctrl = msg->buttons[1];//A键按下：buttons1 1 - -1
+    _roll_ctrl = msg->buttons[2];//B键按下：buttons2 1 - -1
     _input_update = true;
     //RCLCPP_INFO(this->get_logger(), "更新按键值");
 }
@@ -19,15 +22,45 @@ void joy_ctrl::BatteryStatus_callback(const px4_msgs::msg::BatteryStatus::Shared
     _battery_voltage = msg->voltage_filtered_v;
 }
 
+void joy_ctrl::VehicleAttitude_callback(const px4_msgs::msg::VehicleAttitude::SharedPtr msg)
+{
+    Quatf q_att(msg->q[0],msg->q[1],msg->q[2],msg->q[3]);
+    Eulerf euler_att(q_att);
+    _roll = euler_att.phi();
+    _yaw = euler_att.psi();
+    std::cout << "roll: " << _roll << " yaw: " << _yaw << std::endl;
+}
+
+void joy_ctrl::SensorBaro_callback(const px4_msgs::msg::SensorBaro::SharedPtr msg)
+{
+    _depth = (msg->pressure-103000)/10000.0f;
+}
+
 void joy_ctrl::Timer_Control_callback()
 {
-    if(_input_update){
+    float depth_out;
+    float roll_out;
+    float yaw_out;
+    if(_depth_ctrl){
+        depth_out = m_IncPID_depth.IncPIDCalc(_depth,_depth_set,_depth_p,_depth_i,_depth_d);
+        std::cout<<"深度控制:"<<depth_out<<std::endl;
+    } else { depth_out =  -_ctrl_input[1]*_z_max;}
+    if(_roll_ctrl){
+        roll_out = m_IncPID_roll.IncPIDCalc(_roll,_roll_set,_roll_p,_roll_i,_roll_d);
+        std::cout<<"横滚控制:"<<roll_out<<std::endl;
+    } else { roll_out =  0;}
+    if(_yaw_ctrl){
+        yaw_out = m_IncPID_yaw.IncPIDCalc(_yaw,_yaw_set,_yaw_p,_yaw_i,_yaw_d);
+        std::cout<<"航向控制:"<<yaw_out<<std::endl;
+    } else { yaw_out =  -_ctrl_input[0]*_yaw_max;}
+    if(_input_update || _depth_ctrl || _yaw_ctrl || _roll_ctrl){
         _input_update = false;
         _T_max = 0.0067 * _battery_voltage * _battery_voltage + 0.4115* _battery_voltage - 0.4524;
         _T_min = 0.0022 * _battery_voltage * _battery_voltage - 0.4984* _battery_voltage + 1.3815;
+
         Eigen::Matrix<float, 6, 1> Vforce_input;
         Vforce_input << 
-        _ctrl_input[3]*_x_max, _ctrl_input[2]*_y_max, -_ctrl_input[1]*_z_max, 0, _ctrl_input[3]*_x_max * 0.0849, _ctrl_input[0]*_yaw_max;
+        _ctrl_input[3]*_x_max, -_ctrl_input[2]*_y_max, depth_out, roll_out, _ctrl_input[3]*_x_max * 0.0849, yaw_out;
         //std::cout << "force_input: " << Vforce_input.transpose() << std::endl;
         Eigen::Matrix<float, 6, 1> force_input = _power_dis.virtual2real_force(Vforce_input);
         //std::cout << "force_input: " << force_input.transpose() << std::endl;
